24c3a57b859aa540dd13c455eed3c81a03929d01
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2026-01-30 22:26:35 -0800

    comment block

--- global-gameam-2026/Scripts/player.gd
+++ global-gameam-2026/Scripts/player.gd
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Double Jump - RED MASK
+	# Mask 1 = Double Jump
 	if mask_double_jump and Input.is_action_just_pressed("jump") and !is_on_floor():
 		velocity.y = jump_velocity
 		
