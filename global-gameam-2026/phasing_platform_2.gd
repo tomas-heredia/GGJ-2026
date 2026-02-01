7de25329d4323e5810ad778a899fb348f57ad777
Lyra <ecogirl1800@gmail.com>
2026-02-01 01:31:01 -0800

    Fix to phasing platform that did not make platform phasable

---
+++ global-gameam-2026/phasing_platform_2.gd
@@ -0,0 +1,19 @@
+extends StaticBody3D
+
+var count = 0
+var materialTransparent = preload("res://transparentMaterial.tres")
+var materialWhite = preload("res://whiteMaterial.tres")
+
+func _ready() -> void:
+	Signals.connect("Change",_on_character_body_3d_change)
+	$CollisionShape3D.disabled = true
+	$MeshInstance3D.set_surface_override_material(0, materialTransparent)
+
+func _on_character_body_3d_change() -> void:
+	count += 1
+	if count % 2 == 1:
+		$CollisionShape3D.disabled = false
+		$MeshInstance3D.set_surface_override_material(0, materialWhite)
+	else:
+		$CollisionShape3D.disabled = true
+		$MeshInstance3D.set_surface_override_material(0, materialTransparent)
